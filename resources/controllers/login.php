4f1d8d49aeee34f22cf9e86c93f807701c3a242f
Fernando Cortés <fernando.cortes971@gmail.com>
2023-02-26 18:28:11 -0600

    FERNANDO [ADD]| Se creó la funcionalidad del login
    [ADD] | Se creo el archivo login-validations.js en el que se validan los campos y se manda a llamar el archivo login.php por medio de AJAX
    
    [ADD] | Se creó el archivo login.php en donde se crea la sesion del usuario
    
    [UPDATE] | Se actualizó el index para incluir codigo PHP en el

---
+++ resources/controllers/login.php
@@ -0,0 +1,53 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+header("X-debug-Response: Ola k ase*******************");
+
+// Incluir archivo de conexión a la base de datos
+include_once('db.php');
+
+// Verificar si se hizo una solicitud AJAX
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
+
+  // Obtener datos del formulario
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // Conectar a la base de datos
+  $connect = conectar();
+
+  // Preparar la consulta SQL para buscar al usuario por correo electrónico
+  $consulta = "SELECT * FROM users WHERE email = '$email'";
+  $resultado = mysqli_query($connect, $consulta);
+
+  // Verificar si se encontró al usuario
+  if (mysqli_num_rows($resultado) > 0) {
+    // Obtener los datos del usuario
+    $usuario = mysqli_fetch_assoc($resultado);
+    // Verificar si la contraseña ingresada es correcta
+    if (password_verify($password, $usuario['password'])) {
+      // Inicio de sesión correcto
+      session_start();
+      $_SESSION['user_id'] = $usuario['id'];
+      $_SESSION['user_name'] = $usuario['name'];
+      echo 'Inicio de sesión correcto.';
+    } else {
+      // Contraseña incorrecta
+      http_response_code(400); // Establecer código de estado HTTP 400 (Bad Request)
+      echo "La contraseña ingresada es incorrecta.";
+    }
+  } else {
+    // Usuario no encontrado
+    http_response_code(400); // Establecer código de estado HTTP 400 (Bad Request)
+    echo "El correo electrónico ingresado no está registrado en la base de datos.";
+  }
+
+  // Cerrar la conexión a la base de datos
+  cerrarConexion($connect);
+
+} else {
+  // Redireccionar a la página de inicio de sesión
+  header('Location: ../../login.html');
+}
+
+?>
---
+++ resources/js/login-validations.js
@@ -0,0 +1,44 @@
+function validateForm() {
+
+  // Validar campos
+  var email = $("#email").val();
+  var password = $("#password").val();
+  if (email == "" || password == "") {
+    $("#message").html("<p class='error-message'>Todos los campos son obligatorios.</p>");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function loginValidation() {
+  $("#login-form").submit(function(event) {
+    // Evitar que se recargue la página
+    event.preventDefault();
+
+    // Validar el formulario
+    if (validateForm()) {
+      // Crear objeto FormData y agregar campos del formulario
+      var formData = new FormData();
+      formData.append("email", $("#email").val());
+      formData.append("password", $("#password").val());
+
+      // Enviar solicitud AJAX
+      $.ajax({
+        url: "../resources/controllers/login.php",
+        method: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+          $("#message").html(`<p class='success-message'>${response}</p>`);
+          window.location.replace("index.html");
+        },
+        error: function(xhr, status, error) {
+          var errorMessage = xhr.responseText;
+          $("#message").html(`<p class='error-message'>${errorMessage}</p>`);
+        }
+      });
+    }
+  });
+}
