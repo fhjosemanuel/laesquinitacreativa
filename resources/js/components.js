b3903b0e073abee053bf8419e7fbd2608a104b7f
JOSE MANUEL FLORES HERNANDEZ <fhjosemanuel@gmail.com>
2023-03-04 13:14:20 -0600

    Se agrega archivo para el uso de header y footer

---
+++ resources/js/components.js
@@ -0,0 +1,145 @@
+let header =
+`
+<div class="container-fluid">
+    <div class="row py-2 px-xl-5 " style="background-color: rgba(244, 63, 94)">
+        <div class="col-lg-6 d-none d-lg-block">
+            <div class="d-inline-flex align-items-center">
+                <a class="text-white" href="">La</a>
+                <span class="text-muted px-2">|</span>
+                <a class="text-white" href="">Esquinita</a>
+                <span class="text-muted px-2">|</span>
+                <a class="text-white" href="">Creativa</a>
+            </div>
+        </div>
+        <div class="col-lg-6 text-center text-lg-right">
+            <div class="d-inline-flex align-items-center">
+                <a class="text-white px-2" href="">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a class="text-white px-2" href="">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a class="text-white px-2" href="">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+                <a class="text-white px-2" href="">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a class="text-white pl-2" href="">
+                    <i class="fab fa-youtube"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row align-items-center py-3 px-xl-5">
+        <div class="col-lg-3 d-none d-lg-block">
+            <!-- <a href="" class="text-decoration-none">
+                <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">La</span>Papeleria</h1>
+            </a> -->
+        </div>
+        <div class="col-lg-6 col-6 text-left">
+            <form action="">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Buscar por categor\u00EDa o producto">
+                    <div class="input-group-append">
+                        <span class="input-group-text bg-transparent" s>
+                            <i class="fa fa-search" tyle="background-color: rgba(244, 63, 94);"></i>
+                        </span>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-lg-3 col-6 text-right">
+            <a href="" class="btn border">
+                <i class="fas fa-heart text-primary"></i>
+                <span class="badge">0</span>
+            </a>
+            <a href="" class="btn border">
+                <i class="fas fa-shopping-cart text-primary"></i>
+                <span class="badge">0</span>
+            </a>
+        </div>
+    </div>
+</div>
+`;
+let footer =
+`
+<div class="container-fluid text-white mt-5 pt-5" style="background-color: rgba(244, 63, 94);">
+    <div class="row px-xl-5 pt-5">
+        <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
+            <a href="" class="text-decoration-none">
+                <h6 class="mb-4 display-5 font-weight-semi-bold text-white"></span>La esquinita creativa</h6>
+            </a>
+            <p>La papeleria a tu alcance.</p>
+            <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Nezahualc\u00F3yotl, Estado de M\u00E9xico, M\u00E9xico</p>
+            <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>support@niglo.com</p>
+            <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>(+52) 5521331145</p>
+        </div>
+        <div class="col-lg-8 col-md-12">
+            <div class="row">
+                <div class="col-md-4 mb-5">
+                    <h5 class="font-weight-bold text-white mb-4">Categor\u00EDas</h5>
+                    <div class="d-flex flex-column justify-content-start">
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Carpetas</a>
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Agendas</a>
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Mochilas</a>
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Fundas</a>
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Bolsas</a>
+                        <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Manualidades</a>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <h5 class="font-weight-bold text-white mb-4">Productos</h5>
+                    <div class="d-flex flex-column justify-content-start">
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Oficina</a>
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Escolar</a>
+                        <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Ni\u00F1os</a>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5 text-white">
+                    <h5 class="font-weight-bold mb-4 text-white">Cont\u00E1ctanos</h5>
+                    <form action="">
+                        <div class="form-group">
+                            <input type="text" class="form-control border-0 py-4" placeholder="Nombre" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control border-0 py-4" placeholder="Correo"
+                                required="required" />
+                        </div>
+                        <div>
+                            <button class="btn btn-primary btn-block border-0 py-3" type="submit">Suscr\u00EDbete</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row border-top border-light mx-xl-5 py-4">
+        <div class="col-md-6 px-xl-0">
+            <p class="mb-md-0 text-center text-md-left text-dark">
+                &copy; <a class="text-dark font-weight-semi-bold" href="#">www.laesquinitacreativa.test</a>. Todo los derechos reservados. Dise\u00F1ado
+                por
+                <a class="text-dark font-weight-semi-bold" href="#">NIGLO 2023</a><br>
+
+                <!-- All Rights Reserved. Designed
+                by
+                <a class="text-dark font-weight-semi-bold" href="https://htmlcodex.com">HTML Codex</a><br>
+                Distributed By <a href="https://themewagon.com" target="_blank">ThemeWagon</a> -->
+
+            </p>
+        </div>
+        <div class="col-md-6 px-xl-0 text-center text-md-right">
+            <img class="img-fluid" src="public/img/payments.png" alt="">
+        </div>
+    </div>
+</div>
+`;
+
+function headerShow(){
+    let headerHTML = document.getElementById("header");
+    headerHTML.innerHTML = header;
+}
+function footerShow(){
+    let footerHTML = document.getElementById("footer");
+    footerHTML.innerHTML = footer;
+}
