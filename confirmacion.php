5b12e21d12018a05a362c5590fed20e64ef1bff9
Fernando Cortés <fernando.cortes971@gmail.com>
2023-03-05 01:41:14 -0600

    [ADD] | Se añadió la libreria PHPMailer con Composer
    [ADD] | Se creó el archivo .gitignore para agregar las librerias en vendor
    
    [ADD] | Se creó el archivo emailValidation.php en donde se encuentran las funciones responsables de generar el código, configurar el mensaje y los parametros necesarios para enviar el correo electrónico, además de realizar las validaciones pertinentes
    
    [ADD] | Se creó validationEmail.html, en donde se encuentra la plantilla para el correo electrónico
    
    [ADD] | Se creo el archivo confirmacion.php responsable de validar que el código recibido sea el correcto, actualiza la BD si se cumplen las validaciónes pertinentes y modifica la SESSION

---
+++ confirmacion.php
@@ -0,0 +1,56 @@
+<?php
+  include_once('./resources/controllers/db.php');
+
+  $verification_code = $_GET['token'] ?? null;
+  $email = $_GET['email'] ?? null;
+
+  //* Validaciones para evitar inyecciones de código
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    die("El email proporcionado es inválido.");
+  }
+  if (!ctype_alnum($verification_code)) {
+      die("El token de verificación proporcionado es inválido.");
+  }
+
+  // Verificar que la URL de confirmación tiene la estructura correcta
+  $url_parts = parse_url($_SERVER['REQUEST_URI']);
+  if (!isset($url_parts['query']) || empty($verification_code) || empty($email)) {
+      header('Location: ./index.html');
+      exit();
+  }
+
+  // Conectar a la base de datos
+  $connect = conectar();
+  $query = "SELECT * FROM users WHERE email = ?";
+  //* Realiza una consulta preparada, evita inyecciones SQL
+  $stmt = mysqli_prepare($connect, $query);
+  mysqli_stmt_bind_param($stmt, "s", $email);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+  cerrarConexion($connect);
+
+  if (mysqli_num_rows($result) == 1) {
+    $user = mysqli_fetch_assoc($result);
+    if($user['code_validation'] == $verification_code){
+      $id = $user['id'];
+      $connect = conectar();
+      $query = "UPDATE users SET validated = true WHERE id = ?";
+      $stmt = mysqli_prepare($connect, $query);
+      mysqli_stmt_bind_param($stmt, "i", $id);
+      mysqli_stmt_execute($stmt);
+      cerrarConexion($connect);
+      session_start();
+      $_SESSION['user_id'] = $user['id'];
+      $_SESSION['user_name'] = $user['name'];
+      $_SESSION['user_email'] = $user['email'];
+      $_SESSION['validated'] = true;
+      header('Location: ./index.php');
+      exit();
+    }else {
+      //?Se debe enviar un message de que el código no era valido
+      header('Location: ./index.php');
+      die("El código de verificación proporcionado es inválido, validación rechazada");
+    }
+  }
+  exit();
+?>
