223ebd4980f04f8ba1646391241844a736e927e9
Fernando Cortés <fernando.cortes971@gmail.com>
2023-03-08 13:01:35 -0600

    Fernando [ADD] | Se creó el archivo .env para seguridad de las credenciales
    Se actualizaron los archivos con credenciales sensibles para hacer uso de .env

--- resources/controllers/db.php
+++ resources/controllers/db.php
@@ -1,7 +1,13 @@
 <?php
 
 function conectar() {
-  $conexion = mysqli_connect('localhost', 'root', '', 'esquinita');
+  $db_host = getenv('DB_HOST');
+  $db_name = getenv('DB_NAME');
+  $db_user = getenv('DB_USER');
+  $db_password = getenv('DB_PASSWORD');
+
+
+  $conexion = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 
   if (mysqli_connect_errno()) {
     die('Error de conexión a la base de datos: ' . mysqli_connect_error());
